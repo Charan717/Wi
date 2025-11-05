de2d4e9fbb4ce97c856921b2f783ee5b99052bf1
Charan717 <saicharangolkonda34@gmail.com>
2025-11-05 23:29:28 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+public class Swap{
+public static void main(String args[]){
+int temp;
+int a=4;
+int b=7;
+System.out.println("Before swapping a="+a+"b="+b);
+temp=a;
+a=b;
+b=temp;
+System.out.println("After swapping a="+a+ "b="+b);
+}}
